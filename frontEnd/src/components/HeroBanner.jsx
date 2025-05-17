db752c1301007c4452e0141b1cc56032ed2b02f4
Oluwaseun Oloto <oloto.oluwaseun@gmail.com>
2025-05-17 09:31:42 -0700

    banner carousel complete

--- frontEnd/src/components/HeroBanner.jsx
+++ frontEnd/src/components/HeroBanner.jsx
@@ -7,8 +7,41 @@ import TikTok from '../assets/Tiktok.png'
 
 const HeroBanner = () => {
   return (
-    <div className='hero-container' style={{height: "450px", margin: '-900px 0 200px 0'}}>
-      <img className='heroBanner1' style={{height: "450px" }} src={heroImage} alt="Hero Banner" />
+  <>
+  <div style={{marginTop:-750, marginLeft:200}}>
+    
+    <h1 style={{
+          marginLeft: 500, 
+          marginBottom: 20, 
+          textSize: 50,
+          zIndex:1 }}> 
+          Rooted in the vibrant diverse<br />cultures of Africa.
+    </h1>
+    <button style={{
+      marginLeft: 500, 
+      marginBottom: 570, 
+      marginTop:-700,
+      textSize: 50,
+      height: 40,
+      width:300,
+      zIndex:1,
+      color:'white',
+      background:'black',
+      border: 'none',
+      }}> Explore
+    </button>
+    <span style={{
+      backgroundColor:'black', 
+      height:200, 
+      width:200, 
+      zIndex:2,
+      marginTop:-100,
+      }}>
+      </span>
+  </div>
+    
+    <div className='hero-container' style={{ height: "450px", margin: '-900px 0 200px 0', zIndex:-1 }}>
+      <img className='heroBanner1' style={{ height: "450px" }} src={heroImage} alt="Hero Banner" />
       
       <div className='social-icons'>
           <img src={whatsapp} alt="Whatsapp" />
@@ -16,6 +49,7 @@ const HeroBanner = () => {
           <img src={TikTok} alt="Tiktok" />
       </div>
     </div>
+  </>
   )
 }
 
