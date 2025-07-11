f99097b7c8f9257cf66283489dd249df91821f1e
Oluwaseun Oloto <oloto.oluwaseun@gmail.com>
2025-07-10 19:27:13 -0700

    Updated admin and frontEnd

--- tisoraAdmin/src/App.jsx
+++ tisoraAdmin/src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-// import { setUser } from '../redux/adminAuthSlice'; // or wherever it's located
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+
 import AdminAuth from "../pages/AdminAuth"
 import OrdersLanding from '../pages/ordersLanding';
-import { BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 import AdminDashboardLanding from "../pages/adminDashboardLanding";
 // import AdminLayout from '../layout/adminLayout';
+
+import { BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 import Nav from '../components/Nav';
 import Footer from '../components/Footer';
 
--- tisoraAdmin/vite.config.js
+++ tisoraAdmin/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    base: '/admin/',
     plugins: [react()],
     build: { outDir: 'dist' },
   // server: {
